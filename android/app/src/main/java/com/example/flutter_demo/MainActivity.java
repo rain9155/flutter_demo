f1e4e69386201889c731236cdbfeec8bda3be868
rain9155 <jianyu9155@gmail.com>
2021-10-31 18:10:23 +0800

    迁移到androidx、android embedded v2

--- android/app/src/main/java/com/example/flutter_demo/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_demo/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.flutter_demo;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    super.configureFlutterEngine(flutterEngine);
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+    registerBatteryPlugin(flutterEngine);
   }
+
+  private void registerBatteryPlugin(FlutterEngine flutterEngine) {
+    MethodChannel _batteryMethodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), "com.example.flutter_demo/battery");
+    _batteryMethodChannel.setMethodCallHandler(new MethodChannel.MethodCallHandler() {
+      @Override
+      public void onMethodCall(@NonNull MethodCall methodCall, @NonNull MethodChannel.Result result) {
+        if (methodCall.method.equals("getBatteryLevel")) {
+          int batteryLevel = getBatteryLevel();
+          if (batteryLevel != -1) {
+            result.success(batteryLevel);
+          } else {
+            result.error("UNAVAILABLE", "Battery level not available.", null);
+          }
+        } else {
+          result.notImplemented();
+        }
+      }
+    });
+  }
+
+  private int getBatteryLevel() {
+    int batteryLevel;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      BatteryManager batteryManager = (BatteryManager) getSystemService(Context.BATTERY_SERVICE);
+      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+    }else {
+      Intent intent = (new ContextWrapper(getApplicationContext())).registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+      batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+    }
+    return batteryLevel;
+  }
+
 }
